ada4cf499c7fd3c6cb6d4c7349b133641e480232
Nolan <nolanpotier@gmail.com>
2020-03-18 12:15:41 +0100

    Download image

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,23 @@
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
+const fs = require('fs');
+const axios = require('axios').default;
+
+const download_image = (url, image_path, auth) =>
+  axios({
+    url,
+    responseType: 'stream',
+    headers: {'Authorization': auth},
+  }).then(
+    response =>
+      new Promise((resolve, reject) => {
+        response.data
+          .pipe(fs.createWriteStream(image_path))
+          .on('finish', () => resolve())
+          .on('error', e => reject(e));
+      }),
+  );
+
+
 
 exports.sourceNodes = async (
   { actions, ...createNodeHelperFunctions },
@@ -9,12 +28,13 @@ exports.sourceNodes = async (
   // Get data from Confluence
   const response = await search(pluginOptions)
 
+  const { hostname } = pluginOptions;
+
   const baseUrl = response._links.base
   const results = response.results.filter(result => result.type === 'page')
 
   // Parse into nodes and add to GraphQL schema
-  const nodes = results.map(pageResult =>
-    formatPageNode(createNodeHelperFunctions, pageResult, baseUrl)
+  const nodes = results.map(pageResult => formatPageNode(createNodeHelperFunctions, pageResult, baseUrl, hostname, pluginOptions.auth)
   )
 
   nodes.forEach(node => {
@@ -43,18 +63,33 @@ const search = async ({ hostname, auth, cql, limit = 10 }) => {
 const formatPageNode = (
   { createNodeId, createContentDigest },
   result,
-  baseUrl
+  baseUrl,
+  hostname, 
+  auth
 ) => {
+  const slug = slugify(result.title);
+  const re = /<img[^>]+src="?([^"\s]+)"?[^>]*>/g;
+  const results = re.exec(result.body.view.value);
+  var img;
+  if(results) {
+      img = results[1];
+      download_image(img, `static/${slug}.png`, auth);
+  }
+
+  console.log('NODE IMG: ', img);
+
   content = {
     confluenceId: result.id,
     title: result.title,
-    slug: slugify(result.title),
+    slug,
     confluenceUrl: `${baseUrl}${result._links.webui}`,
     createdDate: new Date(result.history.createdDate),
     author: {
       name: result.history.createdBy.displayName,
       email: result.history.createdBy.email,
+      profilePicture: `https://${hostname}${result.history.createdBy.profilePicture.path}`
     },
+    imageHeader: img,
     bodyHtml: result.body.view.value,
     ancestorIds: result.ancestors.map(x => x.id),
   }
